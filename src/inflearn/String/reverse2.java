5c2ecddf22264540157cb87a4d3cc1eaf0c85468
truelightwater <truelightwater@gmail.com>
2023-03-24 14:04:44 +0900

    Inflearn String reverse

---
+++ src/inflearn/String/reverse2.java
@@ -0,0 +1,42 @@
+package inflearn.String;
+
+import java.util.Scanner;
+
+public class reverse2 {
+    public static String solution(String str) {
+        String answer= "";
+
+        char[] s = str.toCharArray();
+        int lt =0, rt=str.length()-1;
+
+        System.out.println(!Character.isAlphabetic(s[0]));
+        System.out.println(!Character.isAlphabetic(s[1]));
+
+        while (lt < rt) {
+            if (!Character.isAlphabetic(s[lt])) {
+                lt++;
+            } else if (!Character.isAlphabetic(s[rt])) {
+                rt--;
+            } else {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+        }
+
+        answer = String.valueOf(s);
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str =  kb.next();
+        System.out.println(solution(str));
+
+    }
+}
+
